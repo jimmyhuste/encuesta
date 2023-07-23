0ecf7019ae9f74a02a0067f97ecb55a53ac3079c
jimmy exantus <jimmyhuste@gmail.com>
2023-07-23 15:21:46 -0400

    se mejora backend

--- src/controllers/canditatosController.js
+++ src/controllers/canditatosController.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage({
     cb(null, './src/public/images'); // Asegúrate de que la ruta de destino sea correcta
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, file.originalname);
   }
 });
 
