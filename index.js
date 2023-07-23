aba892ff4e569edb18128b65c14c82e015235db1
jimmy exantus <jimmyhuste@gmail.com>
2023-07-23 17:19:13 -0400

    update

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const routePersonas = require('./src/router/personas.routes')
 const routeCandidatos = require('./src/router/candidatos.routes')
 const routeVotos = require('./src/router/votos.routes')
 const routeLogin = require('./src/router/login.routes')
-
+require('dotenv').config();
 require("./src/database/db")
 const app = express()
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const PUERTO = 3000;
+
+const PUERTO = process.env.PORT || 3000;
  app.use('/api', routeCandidatos, routePersonas, routeVotos, routeLogin);
 app.use(cors)
 app.use(express.static(path.join(__dirname, 'public/images')));
