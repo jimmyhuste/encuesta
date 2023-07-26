a0cb5b0842730f74f16a7a0c75f3d565ce066e8c
jimmyduoc <ji.exantus@duocuc.cl>
2023-07-26 16:07:47 -0400

    edit final de cors vercel en index

--- index.js
+++ index.js
@@ -23,9 +23,30 @@ app.get('/', (req, res) => {
   res.json({ message: "Api is running!", status: "ok" })
 });
 
-// Habilita CORS con opciones personalizadas
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
 
-app.use(cors({origin: "https://encuesta-nu.vercel.app/"}));
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
+
 
 
 app.listen(PUERTO, () => {
