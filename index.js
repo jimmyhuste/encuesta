8e00cee1a7d603f744ae25cc19d677d7c1303219
jimmyduoc <ji.exantus@duocuc.cl>
2023-07-26 15:10:45 -0400

    se hizo configuracion CORS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cors = require("cors"); 
+const cors = require("cors");
 const path = require('path');
-const bodyParser =  require("body-parser");
+const bodyParser = require("body-parser");
 const routePersonas = require('./src/router/personas.routes')
 const routeCandidatos = require('./src/router/candidatos.routes')
 const routeVotos = require('./src/router/votos.routes')
@@ -14,27 +14,28 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PUERTO = process.env.PORT || 3000;
- app.use( routeCandidatos, routePersonas, routeVotos, routeLogin);
+app.use(routeCandidatos, routePersonas, routeVotos, routeLogin);
 app.use(cors)
 app.use(express.static(path.join(__dirname, 'public/images')));
 
 
 app.get('/', (req, res) => {
-  res.json({message: "Api is running!", status: "ok"})
+  res.json({ message: "Api is running!", status: "ok" })
 });
 
+// Habilita CORS con opciones personalizadas
+app.use(cors());
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://encuesta-front.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization', 'https://encuesta-nu.vercel.app');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+    return res.status(200).json({});
+  }
   next();
 });
 
-  
-  
-  // Habilita CORS con opciones personalizadas
-  app.use(cors());
 
-app.listen(PUERTO, ()=>{
-    console.log("App is runing on port: ", PUERTO)
+app.listen(PUERTO, () => {
+  console.log("App is runing on port: ", PUERTO)
 });
